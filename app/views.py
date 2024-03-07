4c82180710a811d6d52c670d450b957b9f8231c7
Ketan <ketanmangukiya001@gmail.com>
2024-03-07 20:59:40 +0530

    get phone and name

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+import re
 import PyPDF2
 import requests
 from docx import Document
@@ -54,12 +55,40 @@ class PdfView(APIView):
         return '\n'.join(content)
 
 
+    def extract_contact_info(self, text):
+        # Regular expressions to find name, phone number, and email address
+        name_pattern = re.compile(r'([A-Z][a-z]+(?: [A-Z][a-z]+)*)')
+        phone_pattern = re.compile(r'(?:(?:\+?\d{1,3})?(?:-|\s)?)?\d{10}(?:\s?/\s?\d{10})?')
+        email_pattern = re.compile(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b')
+
+        # Find matches
+        name_matches = name_pattern.findall(text)
+        phone_matches = phone_pattern.findall(text)
+        email_matches = email_pattern.findall(text)
+
+        # Assuming the first match found is the primary one
+        name = name_matches[0] if name_matches else None
+        phone = phone_matches[0] if phone_matches else None
+        email = email_matches[0] if email_matches else None
+
+        return name, phone, email
+
     def post(self, request):
         url = request.data.get('url', None)
         file_type = request.data.get('file_type', None)
         if file_type == "pdf":
             data = self.read_pdf(url)
-            return Response({"data":data})
         elif file_type == "docx":
             data = self.read_doc(url)
-            return Response({"data":data})
+
+        try:
+            name, phone, email = self.extract_contact_info(data)
+        except Exception as e:
+            print('Exception is ::>>>', e)
+        response_data = {
+            "name": name,
+            "phone": phone,
+            "email": email,
+            "original_content": data
+        }
+        return Response(response_data)
